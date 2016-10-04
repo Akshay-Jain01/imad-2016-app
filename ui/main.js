d16326eb3e56ad41177ed7bac3231ce0419ce08a
Akshay Jain <akshay.jain.scet@gmail.com>
2016-10-04 18:36:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,7 @@ button.onclick = function() {
 
 //Submit name
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     //Make a request to the server and send the name
@@ -50,6 +49,8 @@ submit.onclick = function() {
     //Not done yet
   };
   //Make the request
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
   request.open('GET','http://akshay-jain01.imad.hasura-app.io/submit-name?name=' + name,true)
   request.send(null);
    
