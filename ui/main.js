98508119898e9455fdd9a6b34687cea1dd735664
Akshay Jain <akshay.jain.scet@gmail.com>
2016-11-27 04:40:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,8 +28,7 @@ submit.onclick = function() {
   var password = document.getElementById('password').value;
   console.log(username);
   console.log(password);
-  var name = nameInput.value;
-  request.open('POST','http://akshay-jain01.imad.hasura-app.io/login' + name,true);
+  request.open('POST','http://akshay-jain01.imad.hasura-app.io/login',true);
   request.setRequestHeader('Content-Type','application/json');
   request.send(JSON.stringify({username: username, password: password}));
    
