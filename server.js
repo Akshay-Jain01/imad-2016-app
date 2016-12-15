ef8b5082ec659f32fc8f14df370d101e6279f4db
Akshay Jain <akshay.jain.scet@gmail.com>
2016-12-15 21:11:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,6 +59,9 @@ function createTemplate(data) {
     return htmlTemplate;
 }
 
+app.get('/ui/profile.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
+});
 app.get('/ui/index.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
